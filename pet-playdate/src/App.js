de90dbb5a8fa5ec63614dc139a06b2e5b6ed88c0
gls1993 <gls1993@yahoo.com>
2023-10-16 19:07:56 -0700

    Updated routes

--- pet-playdate/src/App.js
+++ pet-playdate/src/App.js
@@ -1,12 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import { LoginSignup } from './Components/LoginSignup/LoginSignup';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
 
 function App() {
   return (
     <div>
-      <LoginSignup/>
-
+      <Router>
+        <Routes>
+          <Route path='/loginsignup' Component={LoginSignup} />
+        </Routes>
+      </Router>
     </div>
   );
 }
