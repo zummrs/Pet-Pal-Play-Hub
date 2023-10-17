dd960a7b2bead8252a5d3e2a69b6b7b540362a72
gls1993 <gls1993@yahoo.com>
2023-10-16 17:59:51 -0700

    Change to firebase.js

--- pet-playdate/src/firebase/firebase.js
+++ pet-playdate/src/firebase/firebase.js
@@ -1,21 +1,9 @@
-// Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAuth } from 'firebase/auth';
 import firebase from 'firebase/compat/app'
 import 'firebase/compat/auth'
 import 'firebase/compat/firestore'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAfGmvePlZ3TY-M3Rli58t_vySElrmgg_4",
-//   authDomain: "project-54940.firebaseapp.com",
-//   projectId: "project-54940",
-//   storageBucket: "project-54940.appspot.com",
-//   messagingSenderId: "254437816903",
-//   appId: "1:254437816903:web:a254940c0fc6fd492a0f62"
-// };
 const app = firebase.initializeApp({
     apiKey: "AIzaSyAfGmvePlZ3TY-M3Rli58t_vySElrmgg_4",
     authDomain: "project-54940.firebaseapp.com",
@@ -25,12 +13,5 @@ const app = firebase.initializeApp({
     appId: "1:254437816903:web:a254940c0fc6fd492a0f62"
 })
 
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-
-// // Initialize Firebase Authentication and get a reference to the service
-// const auth = getAuth(app);
-
-// export {auth};
 export const auth = app.auth()
 export default app
